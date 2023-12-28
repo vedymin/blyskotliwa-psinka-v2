08bec210771d5085d0efcafb0d37924e5c063a07
vedymin <michalskimlm@gmail.com>
2023-12-28 14:14:35 +0100

    add texts about hotel

--- src/routes/hotel/+page.svelte
+++ src/routes/hotel/+page.svelte
@@ -36,7 +36,83 @@
 			class='aspect-auto h-[200px] xl:h-[400px] xl:w-[400px] w-[300px] mt-2 border-black bg-contain bg-center bg-no-repeat '
 			style="background-image: url('/img/dog-szkolenia.webp'); " />
 	</div>
-	<div class='flex-1 border-black border mt-10 xl:mt-0'>tekst</div>
+	<div class='flex-1 border-black mt-10 xl:mt-0'>
+		<h1 class='font-gilroySemiBold text-2xl pb-2'>Zapraszamy do naszego hotelu dla psów! 🐾 </h1>
+		<p class='pb-4'>Wybierasz się na wakacje, delegację lub weekend? Potrzebujesz odpowiedniej opieki dla Twojego psa?
+			Sprawdź co
+			mamy dla Ciebie!</p>
+		<h3 class='font-gilroySemiBold text-xl'>Jak wygląda nasz hotel?</h3>
+		<p class='pb-4'>Hotel znajduję się w miejscu naszego zamieszkania, a jego kameralna forma pozwala nam na poświęcenie
+			odpowiedniej
+			ilości czasu i uwagi każdemu naszemu podopiecznemu. Dla naszych gości przygotowaliśmy boksy wewnętrzne, a cały
+			budynek posiada ogrzewanie podłogowe, by zapewnić komfort nawet w najchłodniejsze dni. Psiaki mogą wedle upodobań
+			skorzystać z ogrodzonego terenu, który zapewnia bezpieczne miejsce do swobodnego biegania i zabaw lub spędzać czas
+			wewnątrz chroniąc się przed zimnem lub deszczem.</p>
+		<h3 class='font-gilroySemiBold text-xl'>Dostosowany do Potrzeb</h3>
+		<p class='pb-4'>Pobyt w hotelu rozpoczyna się od zebrania szczegółowych informacji na temat Twojego psa oraz jego
+			codziennej
+			rutyny. Dzięki temu możemy dostosować się do jego rytmu dnia oraz poznać jego upodobania. Podczas pobytu dbamy o
+			zmęczenie fizyczne i umysłowe, dostosowując aktywności do indywidualnych potrzeb psa. Zdrowie i dobrostan każdego
+			podopiecznego są dla nas najwyższym priorytetem, dlatego też zapewniamy opiekę weterynaryjną kiedy zaistnieje taka
+			potrzeba.</p>
+		<h3 class='font-gilroySemiBold text-xl'>Wizyta zapoznawcza</h3>
+		<p class='pb-4'>Oferujemy możliwość wizyty zapoznawczej! Przyjedź z psem przed planowanym pobytem, aby poznał nową
+			przestrzeń i
+			zaznajomił się z naszym personelem. Wizyta pozwoli zminimalizować stres, a pupil zostanie w miejscu oraz pod
+			opieką osób, które już zna. To również czas dla Ciebie na zapoznanie z nami oraz rozwianie wszelkich
+			wątpliwości.</p>
+		<p class='pb-6'>Zaufaj nam – tu twój pies nie tylko znajdzie schronienie, ale też doświadczy wyjątkowej opieki,
+			pasjonatów oraz trenerów psów, dostosowanej do jego unikalnych potrzeb. </p>
+	</div>
+</div>
+<div class='mx-20'>
+	<h1 class='font-gilroySemiBold text-2xl pb-4'>Jak przygotować psa do hotelu?</h1>
+	<p class='pb-2'>
+		Zanim oddasz psa do naszego hotelu, upewnij się, że spakowałeś wszystko, co sprawi, że twój psiak poczuje się jak w
+		domu. Weź legowisko lub ulubiony kocyk, którego zapach pomoże psu poczuć się jak u siebie, ulubione zabawki,
+		przygotuj
+		też odpowiednią ilość ulubionej karmy.
+	</p>
+
+	<p class='pb-2'>
+		Dbamy o bezpieczeństwo wszystkich naszych gości, dlatego ważne jest, aby przynieść ze sobą aktualne dokumenty
+		potwierdzające szczepienia przeciwko wściekliźnie oraz chorobom zakaźnym, a także odrobaczenie potwierdzone przez
+		lekarza weterynarii lub badanie kału na obecność pasożytów. Warto też zabezpieczyć pupila przeciwko pasożytom
+		zewnętrznym. W razie potrzeby zapewniamy opiekę weterynaryjną, żeby zadbać o zdrowie i bezpieczeństwo psów
+		przebywających pod naszą opieką.
+	</p>
+
+	<p class='pb-2'>
+		W naszym hotelu stawiamy na aktywność fizyczną i umysłową, dlatego zachęcamy do zabrania ulubionych smakołyków i
+		gryzaków, które sprawią, że czas spędzony u nas będzie jeszcze bardziej radosny. Każdego psiaka traktujemy
+		indywidualnie, a oferowane mu zajęcia są dostosowane do jego upodobań i charakteru.
+	</p>
+
+	<p class='pb-2'>
+		Nie zapomnij również o smyczy, szelkach lub obroży odpowiednio dopasowanych, tak by psiak podczas spaceru nie mógł
+		ich
+		zdjąć– to istotne, abyśmy mogli wygodnie spacerować z twoim pupilem i zadbać o aktywność fizyczną.
+	</p>
+
+	<p class='pb-2'>
+		Przygotowanie się do pobytu w naszym hotelu dla psów to gwarancja, że Twój pies spędzi czas w przyjemny i bezpieczny
+		sposób. Nasz zespół pasjonatów zwierząt gwarantuje, że każdy pies otrzyma nie tylko profesjonalną opiekę, ale także
+		mnóstwo miłości i uwagi.
+	</p>
+
+	<p class='pb-6'>
+		Liczymy na miłe chwile spędzone z Twoim pupilem!
+	</p>
+
+	<h1 class='font-gilroySemiBold text-xl'>Podsumowanie, czyli co spakować w skrócie:</h1>
+	<ul class='list-disc ml-6'>
+		<li>Książeczka zdrowia potwierdzająca aktualne szczepienia przeciwko wściekliźnie oraz chorobom zakaźnym, a także odrobaczenie lub badanie kału na obecność pasożytów.</li>
+		<li>Zabezpieczenie przeciwko pasożytom zewnętrznym</li>
+		<li>Odpowiednią ilość karmy</li>
+		<li>Smycz, obroża lub szelki</li>
+		<li>Legowisko lub kocyk</li>
+		<li>Mile widziane smakołyki, gryzaki, zabawki</li>
+	</ul>
 </div>
 <div class='p-12'>
 	<Gallery on:click={handleClick}>
